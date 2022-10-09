05a8a2b6a6eb75d0ecda9545e7597ee0d729d2c0
User <harshit.satya@perpetualblock.io>
2022-10-09 15:32:31 +0530

    TS Generic function

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,4 +1,29 @@
 "use strict";
-let mySize = 2;
-console.log(mySize);
+function rolldice(array) {
+    return Math.floor(Math.random() * array.length);
+}
+function getRandom(as, bs) {
+    const a = as[rolldice(as)];
+    const b = bs[rolldice(bs)];
+    return [a, b];
+}
+const names = [
+    "Amelia",
+    "Ava",
+    "Benjamin",
+    "Charlotte",
+    "Elijah",
+    "Emma",
+    "Evelyn",
+    "Harper",
+    "Henry",
+    "Isabella",
+    "James",
+];
+const numbers = [151, 321, 121, 365, 789, 456, 234, 215, 659];
+const ans = getRandom(names, numbers);
+const [randomName, randomNum] = getRandom(names, numbers);
+console.log(ans);
+console.log(randomName, randomNum);
+const texts = ["A", "B"];
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -82,8 +82,186 @@
 
 //////////////////////////////////////////////////
 // Functions
-function calculateTax(income: number) {
-  if (income < 50_000) return income * 1.2;
+// function calculateTax(income: number, taxYear?: number): number {
+//   if ((taxYear || 2022) < 2022) return income * 1.2;
+//   return income * 1.3;
+// }
 
-  return income * 1.3;
+// function calculateTax(income: number, taxYear = 2022): number {
+//   if (taxYear < 2022) return income * 1.2;
+//   return income * 1.3;
+// }
+
+// calculateTax(10000);
+
+//////////////////////////////////////////////////
+// Objects
+// let employee: {
+//   id: number;
+//   name: string;
+// } = { id: 1, name: "" };
+
+// let employee: {
+//   id: number;
+//   name?: string;
+// } = { id: 1 };
+
+// let employee: {
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void;
+// } = {
+//   id: 1,
+//   name: "",
+//   retire: (date: Date) => console.log(date),
+// };
+
+// employee.id = 0; // readonly cannot modify the value
+// employee.name = "Harshit";
+
+//////////////////////////////////////////////////
+// Type Aliases
+// type Employee = {
+//   readonly id: number;
+//   name: string;
+//   retire: (date: Date) => void;
+// };
+
+// let employee: Employee = {
+//   id: 1,
+//   name: "",
+//   retire: (date: Date) => console.log(date),
+// };
+
+//////////////////////////////////////////////////
+// Union Types
+// function kgs2lbs(weight: number | string): number {
+//   // Narrowing
+//   if (typeof weight === "number") return weight * 2.2;
+//   return parseInt(weight) * 2.2;
+// }
+
+//////////////////////////////////////////////////
+// Intersection Types
+// let weight: number & string; // Both a number and a string at the same time but it's not possible
+
+// type Draggable = {
+//   drag: () => void;
+// };
+
+// type Resizable = {
+//   resize: () => void;
+// };
+
+// type UIWidget = Draggable & Resizable;
+
+// let textBox: UIWidget = {
+//   drag: () => {},
+//   resize: () => {},
+// };
+
+//////////////////////////////////////////////////
+// Literals Types
+// type Quantity = 50 | 100;
+// let quantity: Quantity = 100;
+
+// type Metric = "cm" | "inch";
+// let metric: Metric = "cm";
+
+//////////////////////////////////////////////////
+// Nullable Types
+// function greet(name: string | null | undefined): void {
+//   if (name) console.log(`Hello, ${name.toUpperCase()}`);
+//   else console.log("Namaste!");
+// }
+
+// greet(null);
+
+//////////////////////////////////////////////////
+// Optional Chaining
+// type Customer = {
+//   birthDay?: Date;
+// };
+
+// function getCustomer(id: Number): Customer | null | undefined {
+//   return id === 0 ? null : { birthDay: new Date() };
+// }
+
+// let customer = getCustomer(1);
+// Optional property access operator ?.
+// console.log(customer?.birthDay?.getFullYear());
+
+// Optional element access operator
+// customers is an array
+// if(customers !== null && customers !== undefined) customers[0]
+// or
+// customers?.[0]
+
+// Optional call
+// let log: any = (message: string) => console.log(message);
+// let log: any = null;
+// log?.("a");
+
+////////////////////////////////////////////////// Generic //////////////////////////////////////////////////
+// Generic Funtions
+// function getRandom<T>(array: T[]): T {
+//   const diceRoll = Math.floor(Math.random() * array.length);
+//   return array[diceRoll];
+// }
+
+// const names = [
+//   "Amelia",
+//   "Ava",
+//   "Benjamin",
+//   "Charlotte",
+//   "Elijah",
+//   "Emma",
+//   "Evelyn",
+//   "Harper",
+//   "Henry",
+//   "Isabella",
+//   "James",
+// ];
+
+// const randomName = getRandom<string>(names);
+// console.log(randomName, typeof randomName);
+
+// const numbers = [151, 321, 121, 365, 789, 456, 234, 215, 659];
+// const randomNumber = getRandom<number>(numbers);
+// console.log(randomNumber, typeof randomNumber);
+
+//////////////////////////////////////////////////
+// Multiple Generic Parameters
+function rolldice(array: any[]): number {
+  return Math.floor(Math.random() * array.length);
 }
+
+function getRandom<A, B>(as: A[], bs: B[]): [A, B] {
+  const a = as[rolldice(as)];
+  const b = bs[rolldice(bs)];
+  return [a, b];
+}
+
+const names = [
+  "Amelia",
+  "Ava",
+  "Benjamin",
+  "Charlotte",
+  "Elijah",
+  "Emma",
+  "Evelyn",
+  "Harper",
+  "Henry",
+  "Isabella",
+  "James",
+];
+
+const numbers = [151, 321, 121, 365, 789, 456, 234, 215, 659];
+
+const ans = getRandom<string, number>(names, numbers);
+const [randomName, randomNum] = getRandom<string, number>(names, numbers);
+console.log(ans);
+console.log(randomName, randomNum);
+
+// Generic Array Systax
+const texts: Array<string> = ["A", "B"];
